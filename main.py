669cd83f69ee7ab899d4f69295303530e3d8d178
Daniel <148446213+DanielHemgren@users.noreply.github.com>
2024-09-24 17:19:11 +0200

    Relativ path tillagd

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import logging
+import os
 from api import API
 from datacleaner import DataCleaner
 from datasaver import DataSaver
 
+log_directory = 'logs'  # Hardcoded relative path for the log directory
+os.makedirs(log_directory, exist_ok=True)  # Ensure the logs directory exists
+
 logging.basicConfig(
-    filename='C:/Users/Daniel/Documents/ec_utbildning/F_rdjupad_Python/pipeline.log',
+    filename=os.path.join(log_directory, 'pipeline.log'),  # Hardcoded relative path for the log file
     format='[%(asctime)s][%(name)s] %(message)s', 
     datefmt='%Y-%m-%d %H:%M:%S', 
     level=logging.INFO)
+
 
 logger = logging.getLogger(__name__)
 
@@ -34,7 +39,7 @@ def main():
     except Exception as e:
         logger.error(f'Ett fel uppstod under pipeline execution: {e}')
 
-    logger.info('Data pipeline fullbordad')
+    logger.info('mini data pipeline klar')
 
 if __name__ == "__main__":
     main()
